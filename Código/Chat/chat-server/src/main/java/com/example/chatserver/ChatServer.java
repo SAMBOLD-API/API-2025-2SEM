c85c54f1b09dc3624ae4a857ebb83a6b15280498
João Santos <98664551+joaosantos13@users.noreply.github.com>
2025-10-16 18:10:41 -0300

    <docs>: Upload prototipo chat

---
+++ Código/Chat/chat-server/src/main/java/com/example/chatserver/ChatServer.java
@@ -0,0 +1,13 @@
+package com.example.chatserver;
+
+/**
+ * Hello world!
+ *
+ */
+public class ChatServer 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
